2ebb015d26882fcd9f7a9293ddf41248c018fa3b
Romel Vera <romel.vera.cadena@gmail.com>
2017-03-10 13:39:18 -0500

    General Update
    Updated main file and Readme.

--- README.md
+++ README.md
@@ -4,26 +4,34 @@ Scan the files ".asp, .aspx, .php, .js"
 This scanner can trigger false positive, you should have basic programming skills to use this.
 
 #### Features
-- [x] Scan complete pattern
+- [x] Scan by patterns
 - [x] Scan just for base 64 encoded backdoors
 - [x] Can change the default .php extension to others (.asp .aspx .js)
-- [x] Returns the filename and the number of the line of the backdoor.
+- [x] Returns the filename and the line number of the backdoor.
+- [x] Generates Scan Log file.
 
 ### Basic Usage
-Download it, then run it    
+Needs Python 3  
+Download it, then run it.  
+  
+#### On Windows:  
+example for scanning php files in a directory: `find-backdoor.py "H:\Example\Find-Backdoor\testdir"`  
+example for scanning php files base64 only: `find-backdoor.py -b "H:\Example\Find-Backdoor\testdir"`  
+example for scanning aspx files in a directory: `find-backdoor.py -x "C:\inetpub"`
+#### On Linux:  
 example for current directory (php): `./find-backdoor.py .`    
 example for other directory: `./find-backdoor.py /var/www/html`    
-example for javascript file: `./find-backdoor.py -j /var/www/html`    
-example for aspx file: `./find-backdoor.py -x C:\inetpub`
+example for javascript files: `./find-backdoor.py -j /var/www/html`    
 
 ### Built using
-* Python 3.5 - [Link](https://www.python.org/)
+* Python 3.6 - [Link](https://www.python.org/)
 
 ### Tested on:
-Python 3.4 and 3.5
+Python 3.5 and 3.6
 
 ### TO DO
-- Nothing yet...
+- Better Patterns
+- Add more ASP and Javascript patterns
 
 ### License
 MIT License
--- find-backdoor.py
+++ find-backdoor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # Name: find-backdoor.py
 # Description: Utility to scan a file path for encrypted and obfuscated files (PHP)
-# Author: Romel Vera Cadena (romel.vera.cadena@gmail.com) ( https://github.com/RomelSan?tab=repositories )
-# Python: v3.4 , v3.5
-# Date: 10/23/2015
+# Author: Romel Vera ( https://github.com/RomelSan?tab=repositories )
+# Python: v3.4, v3.5, v3.6
+# Date: 2015/10/23
+# Updated Date: 2017/03/10
 
 # Library imports
 import re
@@ -11,16 +12,18 @@ import os
 import glob
 import sys
 import time
+import datetime
 from optparse import OptionParser
 
 #globals
-pattern = '(eval\(|file_put_contents|base64_decode|python_eval|exec\(|passthru|popen|proc_open|pcntl|assert\(|system\(|shell)' #default pattern, this line is not used at all
+pattern = '(eval\(|file_put_contents|base64_decode|edoced_46esab|python_eval|exec\(|passthru|popen|proc_open|pcntl|assert\(|system\(|shell)' #default pattern, this line is not used at all
 file_extension = '.php' #default extension
-
+file_matched = 0
+    
 #def
 def searchbackdoor(fullpath, pattern):
-    
-    webfile = open(fullpath, "r", encoding='utf-8')
+
+    webfile = open(fullpath, "r") #encoding='utf-8'
     line_num = 0
     hit_count = 0
     for line in webfile:
@@ -28,25 +31,16 @@ def searchbackdoor(fullpath, pattern):
         if re.search(pattern, line):  
             hit_count = hit_count + 1 
             print("Filename: " + str(fullpath))
+            scanlog.write("Filename: " + str(fullpath) + "\n")
             print("Pattern Word: " + str(line))
+            scanlog.write("Pattern Word: " + str(line))
             print("Line Number: " + str(line_num) + "\n")
-    print("Total Backdoors: " + str(hit_count) + "\n")
+            scanlog.write("Line Number: " + str(line_num) + "\n\n")
+    print("Total Lines Detected: " + str(hit_count) + "\n")
+    scanlog.write("Total Lines Detected: " + str(hit_count) + "\n\n")
+    print("Check scan_log.txt" + "\n")
     webfile.close()
-	
-def matchbackdoor(fullpath, pattern, encoding='utf-8'):
     
-    webfile = open(fullpath, "r")
-    line_num = 0
-    hit_count = 0
-    for line in webfile:
-        line_num += 1
-        if re.match(pattern, line):  
-            hit_count = hit_count + 1 
-            print("Filename: " + str(fullpath))
-            print("Pattern Word: " + str(line))
-            print("Line Number: " + str(line_num) + "\n")
-    print("Total Backdoors: " + str(hit_count) + "\n")
-    webfile.close()
 #code
 print("""
   __ _           _       _                _       _
@@ -65,13 +59,7 @@ parser.add_option("-b", "--base64",
                      dest="is_base64",
                      default=False,
                      help="Find \"base 64 encodings\" only",)						 
-					 
-parser.add_option("-m", "--match",
-                     action="store_true",
-                     dest="is_match",
-                     default=False,
-                     help="Find exactly pattern matches",)		
-					 
+				 
 parser.add_option("-a", "--asp",
                      action="store_true",
                      dest="is_asp",
@@ -93,9 +81,6 @@ parser.add_option("-j", "--javascript",
 (options, args) = parser.parse_args()
 
 # Manage conflicts
-if options.is_base64 and options.is_match:
-    parser.error("You must select a single option, -b or -m")
-
 if ((options.is_asp or options.is_aspx) and (options.is_javascript)):
     parser.error("You must select a single option, -a or -x or -j")
 	
@@ -125,21 +110,35 @@ if os.path.exists(args[0]) == False:
 #Set Path
 yourpath = args[0]
 
+# Scanning message and prints Date
 print("\nSearching Directory for Backdoors...\n")
 
+scanlog = open("scan_log.txt","w")
+now = datetime.datetime.now()
+print("Date: " + now.strftime('%m/%d/%Y %H:%M') + "\n")
+scanlog.write("Date: " + now.strftime('%m/%d/%Y %H:%M') + "\n\n")
+
+# Actual Scan for each directory and files
 for root, dirs, files in os.walk(yourpath, topdown=False):
     for name in files:
-        #PHP
+        
         if name.endswith(file_extension):
             currentpath = os.path.join(root, name)
             if options.is_base64:
-                pattern = 'base64_decode'
+                pattern = 'base64_decode|edoced_46esab|FromBase64String'
                 searchbackdoor(currentpath,pattern)
 				
-            elif options.is_match:
-                pattern = '(eval\(|file_put_contents|base64_decode|unescape|python_eval|exec\(|passthru|popen|proc_open|pcntl|assert\(|system\(|shell)'
-                matchbackdoor(currentpath,pattern)
-            
-            else:
-                pattern = '(eval\(|file_put_contents|base64_decode|unescape|python_eval|exec\(|passthru|popen|proc_open|pcntl|assert\(|system\(|shell)'
+            elif options.is_aspx :
+                pattern = 'GetDrives|GetLogicalDrives|FromBase64String|VolumeName'
                 searchbackdoor(currentpath,pattern)
+
+            elif options.is_javascript :
+                pattern = 'btoa|atob|base64|decode'
+                searchbackdoor(currentpath,pattern)
+
+            else: #PHP Default
+                pattern = '(eval\(|file_put_contents|base64_decode|edoced_46esab|unescape|python_eval|exec\(|passthru|popen|proc_open|pcntl|assert\(|system\(|shell)'
+                searchbackdoor(currentpath,pattern)
+
+scanlog.close()
+print("\nDone!")
